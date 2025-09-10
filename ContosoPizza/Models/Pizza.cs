1e7b2c634ce50caa0124ec25d530c0d519484dce
Miguel Angel Ayala Castillo <ingeniero.ayala@outlook.com>
2025-09-09 21:19:44 -0500

    feat: modificada la entidad Pizza

--- ContosoPizza/Models/Pizza.cs
+++ ContosoPizza/Models/Pizza.cs
@@ -4,15 +4,15 @@ namespace ContosoPizza.Models;
 
 public class Pizza
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public bool IsGlutenFree { get; set; }
+    [Range(0, double.MaxValue)]
 
     [Required]
-    public string? Name { get; set; }
-    public PizzaSize Size { get; set; }
-    public bool IsGlutenFree { get; set; }
-
-    [Range(0.01, 9999.99)]
-    public decimal Price { get; set; }
+    public int Price { get; set; }
 }
 
 public enum PizzaSize { Small, Medium, Large }
